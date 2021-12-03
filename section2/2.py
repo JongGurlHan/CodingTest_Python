2666fc726ce71b7fbb5baf80c93068b021841d68
JG HAN <gojkhan13@gmail.com>
2021-12-03 20:56:13 +0900

    1203 2번문제 수정

--- section2/2.py
+++ section2/2.py
@@ -5,7 +5,8 @@ T = int(input())
 for t in range(T):
     n, s, e, k = map(int, input().split())
     a = list(map(int, input().split()))
-    print(a)
-
+    a = a[s-1: e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]) )
 
 
