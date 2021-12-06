bdec2cb5c250f0d92c086037537d87d1fffcf234
JG HAN <gojkhan13@gmail.com>
2021-12-06 23:34:17 +0900

    1206 sec2-4

--- section2/3.py
+++ section2/3.py
@@ -1,20 +1,18 @@
 import sys
 sys.stdin=open("input.txt", "rt")
-#n: 카드수, k:번째수
-N, K = map(int, input().split())
+
+n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-res =set()
-
-for i in range(N):
-    for j in range(i+1, N):
-        for m in range(j+1, N):
+res = set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
             res.add(a[i]+a[j]+a[m])
 
-#set()에는 sort가 없어서 다시 리스트로 변환한다.
 res = list(res)
 res.sort(reverse=True)
-print(res[K-1])
+print(res[k-1])
 
 
 
