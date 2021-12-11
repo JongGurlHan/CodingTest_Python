ccd785588e86895f426fbfd50a2db60c4692b07e
JG HAN <gojkhan13@gmail.com>
2021-12-11 23:22:55 +0900

    12.11 sec2-5

--- section2/5.py
+++ section2/5.py
@@ -1,17 +1,25 @@
-n = int(input())
+import sys
+import collections
 
-def sum_num(inputs):
-    result = 0
-    for i in inputs:
-        if i.isdigit():
-            result+=int(i)
-    return result
+#sys.stdin=open("input.txt", "rt")
 
-arr = []
-for i in range(n):
-    a = input()
-    arr.append(a)
+n, m = map(int, input().split())
+cnt = [0]*(n+m+3)
+result = []
 
-arr.sort(key = lambda x:(len(x), sum_num(x), x))
-for i in arr:
-    print(i)
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        result.append(i+j)
+
+
+for num in result:
+    cnt[num] +=1
+
+maxnum = max(cnt)
+
+for i in range(len(cnt)):
+    if cnt[i] == maxnum:
+        print(i, end=' ')
+
+# print(cnt)
+# print(maxnum)
