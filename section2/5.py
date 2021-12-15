27ae470966b6914fe37ae4af25a6139072f73cd6
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2021-12-15 23:51:38 +0900

    1215 다른풀이 업로드

--- section2/5.py
+++ section2/5.py
@@ -1,25 +1,34 @@
 import sys
-import collections
-
-#sys.stdin=open("input.txt", "rt")
+sys.stdin=open("input.txt", "rt")
 
 n, m = map(int, input().split())
 cnt = [0]*(n+m+3)
-result = []
+max = 0 
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        result.append(i+j)
+        cnt[i+j] += 1
 
+#빈도회수의 최대값 찾기
+for i in range(n+m+1):
+    if cnt[i] > max:
+        max = cnt[i]
 
-for num in result:
-    cnt[num] +=1
-
-maxnum = max(cnt)
-
-for i in range(len(cnt)):
-    if cnt[i] == maxnum:
+for i in range(n+m+1):
+    if cnt[i] == max:
         print(i, end=' ')
 
-# print(cnt)
-# print(maxnum)
+# for i in range(1, n+1):
+#     for j in range(1, m+1):
+#         result.append(i+j)
+
+
+# for num in result:
+#     cnt[num] +=1
+
+# maxnum = max(cnt)
+
+# for i in range(len(cnt)): 
+#     if cnt[i] == maxnum:
+#         print(i, end=' ')
+
