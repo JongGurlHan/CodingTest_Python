c559092a42e422668c669758db23b8057e7c9457
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2021-12-17 23:52:11 +0900

    1217 sec6 풀이2

--- section2/6.py
+++ section2/6.py
@@ -4,18 +4,24 @@ sys.stdin=open("input.txt", "rt")
 n = int(input())
 x = list(map(int, input().split()))
 
-
-def digit_sum(x):
-    for i in x:
-        new = str(i) #숫자를 문자열로 변환 123 => '123'
-        add = 0
-        for j in range(len(new)):
-            add += int(new[j])
+#각 자연수의 자릿수의 합을 구하는 함수
+def digit_sum(num):    
+    str_num = str(num) #숫자를 문자열로 변환 123 => '123'
+    add = 0
+    for i in range(len(str_num)):
+        add += int(str_num[i])
     return add
 
-max =0
-if add > max:
-    max = add
-    
+max = 0
+result = 0
+
+for i in range(len(x)):
+    if digit_sum(x[i]) > max:
+        max = digit_sum(x[i])
+        result = x[i]
+
+print(result)
+
+
    
 
