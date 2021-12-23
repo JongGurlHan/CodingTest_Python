de8c800344a20c6eb34991b54508660c84dc72a5
JongGurlHan <gojkhan13@gmail.com>
2021-12-23 19:08:22 +0900

    12.23 sec2-9 3개 값이 모두 같을때

--- section2/9.py
+++ section2/9.py
@@ -1,8 +1,28 @@
 import sys
-from scipy import stats
 sys.stdin=open("H:/Study/CodingTest_Python/input.txt", "rt")
 
 n = int(input())
+
+max = 0
+price = 0
+
+for _ in range(n):
+    
+    a = list(map(int, input().split())) #주사위 눈 3개 가져오기  
+    if a[0] == a[1] == a[2]:
+        price = 10000 + a[0]*1000
+        print(price)
+        
+    if price > max:
+        max = price       
+            
+        
+print(max)
+
+# testlist = [1 , 2, 2]
+# print(set(testlist))
+# print(stats.mode(testlist))
+
 
 # def prizeMoney(a):
 #     res = 0
@@ -10,17 +30,3 @@ n = int(input())
 #         res = 10000 + a[0]*1000
 #     elif len(set(a)) == 2: #2개 값이 같을때
 #         ma
-        
-    
-
-        
-    
-
-
-for _ in range(n):
-    a = list(map(int, input().split())) #주사위 눈 3개 가져오기
-    print(a)
-
-testlist = [1 , 2, 2]
-print(set(testlist))
-print(stats.mode(testlist))
