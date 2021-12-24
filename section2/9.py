84ddf107553b012f8eb1157e3fd56f4503c7dc58
JG HAN <gojkhan13@gmail.com>
2021-12-24 16:57:05 +0900

    1224 sec2-9 다른 풀이

--- section2/9.py
+++ section2/9.py
@@ -1,32 +1,26 @@
 import sys
-sys.stdin=open("H:/Study/CodingTest_Python/input.txt", "rt")
+sys.stdin=open("input.txt", "rt")
 
 n = int(input())
 
-max = 0
+maxPrice = 0
 price = 0
 
-for _ in range(n):
-    
+for _ in range(n):    
     a = list(map(int, input().split())) #주사위 눈 3개 가져오기  
     if a[0] == a[1] == a[2]:
         price = 10000 + a[0]*1000
-        print(price)
+    elif a[0] == a[1] != a[2]:
+        price = 1000 + a[0]*100
+    elif a[1] == a[2] != a[0]:
+        price = 1000 + a[0]*100
+    elif a[0] == a[2] != a[1]:
+        price = 1000 + a[0]*100
+    else:
+        price = max(a)*100
         
-    if price > max:
-        max = price       
+    if price > maxPrice:
+        maxPrice = price   
             
         
-print(max)
-
-# testlist = [1 , 2, 2]
-# print(set(testlist))
-# print(stats.mode(testlist))
-
-
-# def prizeMoney(a):
-#     res = 0
-#     if len(set(a)) == 1: #3개 값이 모두 같을때
-#         res = 10000 + a[0]*1000
-#     elif len(set(a)) == 2: #2개 값이 같을때
-#         ma
+print(maxPrice)
